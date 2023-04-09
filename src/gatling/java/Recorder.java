8437eef395118569c1f298fd9da470fda04e0d27
nguquen <thienlduc@gmail.com>
2023-04-09 15:57:33 +0700

    Add gatling

---
+++ src/gatling/java/Recorder.java
@@ -0,0 +1,16 @@
+import io.gatling.recorder.GatlingRecorder;
+import io.gatling.recorder.config.RecorderPropertiesBuilder;
+import scala.Option;
+
+public class Recorder {
+
+  public static void main(String[] args) {
+    RecorderPropertiesBuilder props = new RecorderPropertiesBuilder()
+        .simulationsFolder(IDEPathHelper.gradleSourcesDirectory.toString())
+        .resourcesFolder(IDEPathHelper.gradleResourcesDirectory.toString())
+        .simulationPackage("com.leapxpert.usermanagement")
+        .simulationFormatJava();
+
+    GatlingRecorder.fromMap(props.build(), Option.apply(IDEPathHelper.recorderConfigFile));
+  }
+}
---
+++ src/gatling/java/com/leapxpert/usermanagement/HelloSimulation.java
@@ -0,0 +1,29 @@
+package com.leapxpert.usermanagement;
+
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+public class HelloSimulation extends Simulation {
+
+  HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080");
+
+  ChainBuilder getHello = exec(
+      http("Hello").get("/v1/hello").check(status().is(200))
+  );
+
+  ScenarioBuilder scenario = scenario("HelloSimulation").exec(getHello);
+
+  {
+    setUp(
+        scenario.injectOpen(atOnceUsers(100))
+    ).protocols(httpProtocol);
+  }
+}
