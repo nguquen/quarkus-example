580e16e92c367f0656f67041f71d44d860164afc
nguquen <thienlduc@gmail.com>
2023-04-09 13:27:40 +0700

    Initial commit

---
+++ src/native-test/java/com/leapxpert/usermanagement/resource/HelloResourceIT.java
@@ -0,0 +1,8 @@
+package com.leapxpert.usermanagement.resource;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourceIT extends HelloResourceTest {
+    // Execute the same tests but in packaged mode.
+}
